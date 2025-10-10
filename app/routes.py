3716a95bcc547043219d06c01fa8856c0e36d46f
Whit Waltman <whitwaltman@mailbox.org>
2025-10-10 12:43:33 -0700

    add basic template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Whit Waltman'}
+    return render_template('index.html', title='codeblog', user=user)
