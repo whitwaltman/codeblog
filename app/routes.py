cecfb1df11f9156133ab4d86dd8e02014fec822e
Whit Waltman <whitwaltman@mailbox.org>
2025-12-03 12:59:26 -0800

    add form functionality

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -41,9 +41,14 @@ def index():
             'tags': ['git']
         }
     ]
-    return render_template('index.html', title='codeblog', user=user, posts=posts)
+    return render_template('index.html', user=user, posts=posts)
 
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
