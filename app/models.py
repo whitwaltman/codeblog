cbeee6f45cf554307fa6bb0282a3957874ba7824
Whit Waltman <whitwaltman@mailbox.org>
2025-12-03 13:21:38 -0800

    add new model, apply db migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as alc
 import sqlalchemy.orm as orm
@@ -8,6 +9,18 @@ class User(db.Model):
     username: orm.Mapped[str] = orm.mapped_column(alc.String(64), index=True, unique=True)
     email: orm.Mapped[str] = orm.mapped_column(alc.String(120), index=True, unique=True)
     password_hash: orm.Mapped[Optional[str]] = orm.mapped_column(alc.String(256))
+    posts: orm.WriteOnlyMapped['Post'] = orm.relationship(back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    # currently has a 140 character limit imposed
+    body: orm.Mapped[str] = orm.mapped_column(alc.String(140))
+    timestamp: orm.Mapped[datetime] = orm.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: orm.Mapped[int] = orm.mapped_column(alc.ForeignKey(User.id), index=True)
+    author: orm.Mapped[User] = orm.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
