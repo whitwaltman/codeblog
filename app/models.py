c7ec6d21fa4b93b60e09aea6992c271dc8814dcb
Whit Waltman <whitwaltman@mailbox.org>
2025-12-03 13:27:23 -0800

    create shell context environment, add password hashing

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from typing import Optional
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as alc
 import sqlalchemy.orm as orm
 from app import db
@@ -13,6 +14,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
--- codeblog.py
+++ codeblog.py
@@ -1,1 +1,8 @@
-from app import app
+import sqlalchemy as alc
+import sqlalchemy.orm as orm
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'alc': alc, 'orm': orm, 'db': db, 'User': User, 'Post': Post}
